360f895c1b6ee92c9c0cfc6ad54da294644a6ad4
ShahJamal6160 <shahj6160@gmail.com>
2024-12-13 23:36:48 +0600

    added application section

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
@@ -33,13 +33,40 @@ async function run() {
         // jobs related  apis
 
         const jobsCollection = client.db('jobPortal').collection('jobs');
+        const jobsApplicationCollection = client.db('jobPortal').collection('job-application');
 
         // সব ডাটা লোড করার জন্য -১
-        app.get('/jobs', async(req, res)=>{
+        app.get('/jobs', async (req, res) => {
             const cursor = jobsCollection.find();
             const result = await cursor.toArray();
             res.send(result);
         })
+
+        // shown details 
+
+        app.get('/jobs/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await jobsCollection.findOne(query);
+            res.send(result);
+        })
+
+        // job application apis
+
+        app.get('/job-application', async (req, res) => {
+            const email = req.query.email;
+            const query = { applicant_email: email }
+            const result = await jobsApplicationCollection.find(query).toArray();
+            res.send(result);
+        })
+
+        app.post('/job-application', async (req, res) => {
+            const application = req.body;
+            const result = await jobsApplicationCollection.insertOne(application)
+            res.send(result);
+
+        })
+
 
 
 
